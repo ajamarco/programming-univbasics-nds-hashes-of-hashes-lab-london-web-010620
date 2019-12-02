fc0e8159316494d94e4c6b1be00a17958bdb2ab2
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-02 16:17:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,6 +45,11 @@ def naming_system
       :sub_category => {
         :sub_category => {
           :label => "Order"
+          :sub_category => {
+            :sub_category => {
+              :label => "Genus"
+            }
+          }
         }
       }
     }
